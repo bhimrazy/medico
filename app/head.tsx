62b5b0d8b69d1b566ffa05987d8078190a65f34f
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2022-12-11 11:56:16 +0000

    Added landing page

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Medico | A Healthcare App</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Medico | A Healthcare App." />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
